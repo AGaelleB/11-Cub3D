1dce030f9d7f7353340d3b6b451a1868034128b4
Battone1 <b.fresquet06@gmail.com>
2024-01-12 10:31:05 +0100

    maj map 5.0 / fix pos player end/begin

--- srcs/parsing/master_pars.c
+++ srcs/parsing/master_pars.c
@@ -6,7 +6,7 @@
 /*   By: bfresque <bfresque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 13:40:13 by abonnefo          #+#    #+#             */
-/*   Updated: 2024/01/11 15:20:51 by bfresque         ###   ########.fr       */
+/*   Updated: 2024/01/12 10:22:40 by bfresque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,17 +45,17 @@ int	master_verif_maps(t_parser *parser)
 		return (err("Error\nInvalid map\n"));
 	}
 	parms_map_size(parser);
-	if (verif_char(parser->map) || verif_pos_player(parser))
-	{
-		free_tab(parser->tab);
-		free_tab(parser->map);
-		return (err("Error\nInvalid char\n"));
-	}
 	if (verif_zero_close(parser))
 	{
 		free_tab(parser->tab);
 		free_tab(parser->map);
 		return (err("Error\nMap isn't close\n"));
+	}
+	if (verif_char(parser->map) || verif_pos_player(parser))
+	{
+		free_tab(parser->tab);
+		free_tab(parser->map);
+		return (err("Error\nInvalid char\n"));
 	}
 	if (verif_space_close(parser))
 	{
--- srcs/parsing/pars_start_player.c
+++ srcs/parsing/pars_start_player.c
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   pars_start_player.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abonnefo <abonnefo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bfresque <bfresque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 15:40:33 by abonnefo          #+#    #+#             */
-/*   Updated: 2023/12/22 17:33:32 by abonnefo         ###   ########.fr       */
+/*   Updated: 2024/01/12 10:30:36 by bfresque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+
+int	verif_first_param(t_parser *parser, int i)
+{
+	if (parser->map[i][0] == 'N' || parser->map[i][0] == 'S'
+		|| parser->map[i][0] == 'E' || parser->map[i][0] == 'W')
+		return (1);
+	return (0);
+}
 
 int	verif_inside_pos_player(t_parser *parser, int i, int j)
 {
@@ -19,22 +27,23 @@ int	verif_inside_pos_player(t_parser *parser, int i, int j)
 
 	prev_row_len = 0;
 	next_row_len = 0;
-	if (parser->map[i][0] == 'N' || parser->map[i][0] == 'S'
-		|| parser->map[i][0] == 'E' || parser->map[i][0] == 'W')
-		return (1);
 	if (i > 0)
 		prev_row_len = ft_strlen(parser->map[i - 1]);
 	if (i < parser->map_height - 1)
 		next_row_len = ft_strlen(parser->map[i + 1]);
-	if ((i > 0 && j < prev_row_len && (parser->map[i - 1][j] == ' '
-			|| !parser->map[i - 1][j]))
-			|| (i < parser->map_height - 1 && j < next_row_len
-			&& (parser->map[i + 1][j] == ' ' || !parser->map[i + 1][j]))
-			|| (j > 0 && parser->map[i][j - 1] == ' ')
-			|| (j < parser->map_width - 1 && parser->map[i][j + 1] == ' '))
+	if ((i > 0 && j < prev_row_len && (!(parser->map[i - 1][j] == '1')
+			&& !(parser->map[i - 1][j] == '0')))
+			|| ((i < parser->map_height - 1) && j < next_row_len
+			&& (!(parser->map[i + 1][j] == '1')
+			&& !(parser->map[i + 1][j] == '0')))
+			|| (j > 0 && (!(parser->map[i][j - 1] == '1')
+			&& !(parser->map[i][j - 1] == '0')))
+			|| ((j < parser->map_width - 1)
+			&& (!(parser->map[i][j + 1] == '1')
+			&& !(parser->map[i][j + 1] == '0'))))
 		return (1);
-	if ((i > 0 && j >= prev_row_len)
-		|| (i < parser->map_height - 1 && j >= next_row_len))
+	if ((i > 0 && j >= prev_row_len) || (i < parser->map_height - 1
+			&& j >= next_row_len) || verif_first_param(parser, i))
 		return (1);
 	return (0);
 }
